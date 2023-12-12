c0012ef1a4f8c980a373d704b849a4c586f52cf1
Groszi01 <oruak@protonmail.com>
2023-12-12 08:35:25 +0100

    Function meghívása

--- main.js
+++ main.js
@@ -9,3 +9,5 @@ mama("Szeretem a fagyit")
 function hosszusag(tik) {
     console.log("A tik változó hosszúsága: " + tik.length, "A tik váltózó csupa kis betűvel: " + tik.toLowerCase())
 }
+
+hosszusag("TETTEDETETTEKTETTESÉNEKTETTESE")
